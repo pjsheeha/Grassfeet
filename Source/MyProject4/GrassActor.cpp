5391cd5d8b2c5639f891ad891eddfd16fe2720f5
Xiang Fan <sfanxiang@gmail.com>
2019-10-13 16:47:11 -0400

    GrassActor: tmp

--- Source/MyProject4/GrassActor.cpp
+++ Source/MyProject4/GrassActor.cpp
@@ -34,16 +34,33 @@ void AGrassActor::Tick(float DeltaTime)
 	auto pawn = controller->GetPawn();
 	if (!pawn) return;
 
-	UClass* graph = FindObject<UClass>(ANY_PACKAGE, L"/Game/TwinStickBP/Blueprints/PlanetGraph.PlanetGraph_C");
-	UArrayProperty* prop = FindField<UArrayProperty>(graph->GetClass(), L"non_hotloaded_graph");
-	if (!prop) return;
-	FScriptArray *script_array = prop->GetPropertyValuePtr_InContainer(graph);
+	GF_LOG(L"GA: 1111111111");
+
+	UClass* graph_class = FindObject<UClass>(ANY_PACKAGE, L"/Game/TwinStickBP/Blueprints/PlanetGraph.PlanetGraph_C");
+	GF_LOG(L"GA: 2222222222");
+
+	FScriptArray* script_array = nullptr;
+
+	for (TFieldIterator<UProperty> it(graph_class); it; ++it)
+	{
+		UProperty *prop = *it;
+		if (prop->GetName() == L"non_hotloaded_graph") {
+			UArrayProperty *array = Cast<UArrayProperty>(prop);
+			script_array = array->GetPropertyValuePtr_InContainer(graph_class);
+			if (!script_array) return;
+
+			break;
+		}
+	}
+
 	if (!script_array) return;
+
+	GF_LOG(L"GA: 3333333333");
 
 	// This is ugly, but by design this is how you do it in Unreal.
 	auto &array = *reinterpret_cast<TArray<FPoint>*>(script_array);
 	GF_LOG(L"LEN:      %d", array.Num());
 
 	UClass* grass = FindObject<UClass>(ANY_PACKAGE, L"/Game/TwinStickBP/Blueprints/Grass.Grass_C");
-	world->SpawnActor<UObject>(grass, pawn->GetTransform());
+	//world->SpawnActor<UObject>(grass, pawn->GetTransform());
 }
